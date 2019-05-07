58f0c788c8e7b0b6fdef8aa3dc56518d317fca28
AIPC <grillnov@gmail.com>
2019-05-06 21:28:01 -0400

    Added 2D experiments

--- TestPlot2D.m
+++ TestPlot2D.m
@@ -12,8 +12,9 @@ function TestPlot2D
     estimatedCovX = cov(xSeq);
     estimatedCovY = cov(ySeq);
     
-    [a, b] = Core(estimatedX, estimatedY, estimatedCovX, estimatedCovY);
+    [a, b] = Core(estimatedX', estimatedY', estimatedCovX, estimatedCovY);
     
-    plot(xSeq);
-    plot(ySeq);
+    sz = 4.0;
+    scatter(xSeq(:,1), xSeq(:,2), sz, 'MarkerEdgeColor',[.5 0 .5], 'MarkerFaceColor',[.7 0 .7],'LineWidth',1.5);
+    scatter(ySeq(:,1), ySeq(:,2), sz, 'MarkerEdgeColor',[0 .5 .5], 'MarkerFaceColor',[0 .7 .7],'LineWidth',1.5);
 end
