6cd5370b8617c46fe4d2b771b6d97c86db1854a3
Yulai Weng <30909007+SeraphWeng@users.noreply.github.com>
2019-05-06 20:51:09 -0400

    Add files via upload

--- Test.m
+++ Test.m
@@ -12,3 +12,14 @@ cov_c2 = cov(Cluster_2(1,:),Cluster_2(2,:));
 mu_c1 = mean(Cluster_1,2);
 mu_c2 = mean(Cluster_2,2);
 [a, b] = Core(mu_c1, mu_c2, cov_c1, cov_c2);
+
+
+scatter(Cluster_1(1,:),Cluster_1(2,:),5,'m','filled')
+hold on
+scatter(Cluster_2(1,:),Cluster_2(2,:),5,'g','filled')
+
+min_x = min([Cluster_1(1,:),Cluster_2(1,:)]);
+max_x = max([Cluster_1(1,:),Cluster_2(1,:)]);
+x = linspace(min_x,max_x, 1000);
+y = (b-a(1)*x)/a(2);
+plot(x,y,'b');
