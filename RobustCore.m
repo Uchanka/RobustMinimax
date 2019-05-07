6fa11a6a924228fcb89fdec3dbb8ed3964b35eaa
AIPC <grillnov@gmail.com>
2019-05-07 19:30:13 -0400

    Implementing robustness

---
+++ RobustCore.m
@@ -0,0 +1,87 @@
+%Robust core function in case there're estimation error
+%@a: Normal vector of the hyperplane
+%@b: Scalar of the hyperplane
+%@xBar: Estimated expectation of x
+%@yBar: Estimated expectation of y
+%@xCov: Estimated covariance matrix of x
+%@yCov: Estimated covariance matrix of y
+function [a, b] = RobustCore(xBar, yBar, xCov, yCov)
+
+    % ========CONSTANTS========
+    % Tolerance threshould
+    tolerance = 1e-5;
+    % Delta value for the least-square
+    delta = 1e-6;
+    % Maximum iteration amount
+    maxIteration = 100;
+    % Use relative error
+    relative = true;
+
+    % ========PREPARATIONS========
+    % \Bar{x} - \Bar{y}
+    xBarMyBar = xBar - yBar;
+    a_0 = xBarMyBar / Squared(xBarMyBar);
+    % F orthogonal to \Bar{x} - \Bar{y}
+    F = GenerateOrthogonal(xBarMyBar);
+    %confirmF(F, xBarMyBar)
+    % G, H matrices
+    G = transpose(F) * xCov * F;
+    H = transpose(F) * yCov * F;
+    % g, h vector
+    g = transpose(F) * xCov * a_0;
+    h = transpose(F) * yCov * a_0;
+    [n, ~] = size(xBarMyBar);
+    
+    % ========INIT========
+    a_k = zeros(n);
+    % beta_1 = 1, eta_1 = 1, k = 1
+    beta_k = 1; eta_k = 1; k = 1;
+    % Absolute threshold
+    betaEtasum = (beta_k + eta_k);
+    % Relative threshold
+    threshold = 1;
+    % Identity delta
+    deltaI = delta * eye(n - 1);
+    
+    % ========LOOP========
+    while and(k < maxIteration, threshold > tolerance)
+        % Building the least squares matrix
+        M_LS = (1 / beta_k) * G + (1 / eta_k) * H + deltaI;
+        b_LS = -(1 / beta_k) * g - (1 / eta_k) * h;
+        % Solving for a
+        u_k = M_LS \ b_LS;
+        a_k = a_0 + F * u_k;
+        
+        % Updating beta and eta
+        beta_kup = sqrt(transpose(a_k) * xCov * a_k);
+        eta_kup = sqrt(transpose(a_k) * yCov * a_k);
+        
+        % Convergence criterion
+        betaEtasum_up = beta_kup + eta_kup;
+        if (relative)
+            % Use relative error
+            threshold = abs(betaEtasum_up - betaEtasum) / betaEtasum;
+        else
+            % Use absolute error
+            threshold = betaEtasum_up;
+        end
+        
+        % Update the parameters
+        beta_k = beta_kup;
+        eta_k = eta_kup;
+        betaEtasum = betaEtasum_up;
+        k = k + 1;
+    end
+    
+    % ========RETURN========
+    a = a_k;
+    b = transpose(a_k) * xBar - (beta_k) / (betaEtasum);
+    kappa = 1 / betaEtasum;
+    alpha = kappa^2 / (1 + kappa^2);
+    disp(alpha);
+end
+
+% Utility function.
+function d2 = Squared(input)
+    d2 = transpose(input) * input;
+end
--- TestPlot2D.m
+++ TestPlot2D.m
@@ -2,8 +2,8 @@
 function TestPlot2D
     mX = [-1 1];
     mY = [2, 2];
-    covX = [1.5 1.2; 1.2 2];
-    covY = [2.5 1.5; 1.5 2];
+    covX = [1.1 1.2; 1.2 1.4];
+    covY = [1.2 1.1; 1.1 1.4];
     xSeq = mvnrnd(mX, covX, 40);
     ySeq = mvnrnd(mY, covY, 40);
     
@@ -14,7 +14,11 @@ function TestPlot2D
     
     [a, b] = Core(estimatedX', estimatedY', estimatedCovX, estimatedCovY);
     
-    sz = 4.0;
+    sz = 2.0;
     scatter(xSeq(:,1), xSeq(:,2), sz, 'MarkerEdgeColor',[.5 0 .5], 'MarkerFaceColor',[.7 0 .7],'LineWidth',1.5);
+    hold on
     scatter(ySeq(:,1), ySeq(:,2), sz, 'MarkerEdgeColor',[0 .5 .5], 'MarkerFaceColor',[0 .7 .7],'LineWidth',1.5);
+    
+    x = -2 : 3;
+    plot(x, -(a(1)/a(2)) * x + b);
 end
