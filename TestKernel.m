a53635f22f38715ab7bcb5cd45950b90e07c2a57
AIPC <grillnov@gmail.com>
2019-05-08 02:00:40 -0400

    Found a kernel that's working

--- TestKernel.m
+++ TestKernel.m
@@ -1,29 +1,62 @@
 % A simple kernelization: (x1, x2) -> (x1x1, x1x2, x2x1, x2x2)
 function TestKernel
-    mX = [-2 -2]';
-    mY = [2 2]';
-    covX = [1.4 0.4; 0.4 1.2];
-    covY = [1.2 1.15; 1.15 1.4];
     % Testing
     sampleSize = 50000;
-    xSeq = mvnrnd(mX', covX, sampleSize);
-    ySeq = mvnrnd(mY', covY, sampleSize);
-    % High dimensional
-    xHDSeq = zeros(4, sampleSize);
-    yHDSeq = zeros(4, sampleSize);
-    for i = 1 : sampleSize
-        rX = xSeq(i, :)';
-        rY = ySeq(i, :)';
+    xSeq = zeros(sampleSize, 2);
+    ySeq = zeros(sampleSize, 2);
+    
+    countX = 0;
+    countY = 0;
+    while and(countX < sampleSize, countY < sampleSize)
+        ptX = -2.0 + 4.0 * rand();
+        ptY = -2.0 + 4.0 * rand();
+        % y = 2 - x^2
+        if (2 - ptX^2 > ptY)
+            if countX < sampleSize
+                countX = countX + 1;
+                xSeq(countX, :) = [ptX ptY]';
+            end
+        else
+            if countY < sampleSize
+                countY = countY + 1;
+                ySeq(countY, :) = [ptX ptY]';
+            end
+        end
     end
     
+    % High dimensional
+    [~, nHD] = size(getKernelized(xSeq(1, :)));
+    xHDSeq = zeros(sampleSize, nHD);
+    yHDSeq = zeros(sampleSize, nHD);
+    for i = 1 : sampleSize
+        cX = xSeq(i, :)';
+        cY = ySeq(i, :)';
+        xHDSeq(i, :) = getKernelized(cX)';
+        yHDSeq(i, :) = getKernelized(cY)';
+    end
+    
+    mX = mean(xSeq)';
+    mY = mean(ySeq)';
+    covX = cov(xSeq);
+    covY = cov(ySeq);
+    
+    mXHD = mean(xHDSeq)';
+    mYHD = mean(yHDSeq)';
+    covXHD = cov(xHDSeq);
+    covYHD = cov(yHDSeq);
+    
+    [a, b] = Core(mX, mY, covX, covY);
+    % Original
     misClassifiedX = 0;
     misClassifiedY = 0;
     for i = 1 : sampleSize
         bX = a' * xSeq(i, :)';
-        bY = a' * ySeq(i, :)';
         if bX < b
             misClassifiedX = misClassifiedX + 1;
         end
+    end
+    for i = 1 : sampleSize
+        bY = a' * ySeq(i, :)';
         if bY > b
             misClassifiedY = misClassifiedY + 1;
         end
@@ -31,7 +64,33 @@ function TestKernel
     misClassifiedRateX = misClassifiedX / (sampleSize);
     misClassifiedRateY = misClassifiedY / (sampleSize);
     worstMisClassifiedRate = max(misClassifiedRateX, misClassifiedRateY);
-    disp('Actual misclassificatoin probablity:');
+    disp('Actual misclassification probablity:');
+    disp(worstMisClassifiedRate);
+    
+    [aHD, bHD] = Core(mXHD, mYHD, covXHD, covYHD);
+    % HD
+    misClassifiedX = 0;
+    misClassifiedY = 0;
+    for i = 1 : sampleSize
+        xRow = xSeq(i, :)';
+        xRowHD = getKernelized(xRow);
+        bXHD = aHD' * xRowHD';
+        if bXHD < bHD
+            misClassifiedX = misClassifiedX + 1;
+        end
+    end
+    for i = 1 : sampleSize
+        yRow = ySeq(i, :)';
+        yRowHD = getKernelized(yRow);
+        bYHD = aHD' * yRowHD';
+        if bYHD > bHD
+            misClassifiedY = misClassifiedY + 1;
+        end
+    end
+    misClassifiedRateX = misClassifiedX / (sampleSize);
+    misClassifiedRateY = misClassifiedY / (sampleSize);
+    worstMisClassifiedRate = max(misClassifiedRateX, misClassifiedRateY);
+    disp('Actual misclassification probablity in high dimension:');
     disp(worstMisClassifiedRate);
     
     sz = 2.0;
@@ -41,7 +100,19 @@ function TestKernel
     
     [maxEleX, ~] = max(abs(xSeq(:, 1)));
     [maxEleY, ~] = max(abs(ySeq(:, 1)));
-    maxEle = max(maxEleX, maxEleY);
+    maxEle = ceil(max(maxEleX, maxEleY));
     xPlot = - maxEle : maxEle;
     plot(xPlot, -(a(1)/a(2)) * xPlot + (b / a(2)));
+    daspect([1 1 1]);
+end
+
+function aKer = getKernelized(input)
+    x1 = input(1);
+    x2 = input(2);
+    aKer = gaussKer(x1, x2);
+end
+
+function aKer = gaussKer(x1, x2)
+    sqrt2 = sqrt(2);
+    aKer = [x2^2 x1^2 sqrt2 * x2 * x1 sqrt2 * x1 * x2 sqrt2 * x2 sqrt2 * x1];
 end
